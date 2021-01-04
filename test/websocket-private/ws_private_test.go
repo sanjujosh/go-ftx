10d45674730d24810c2d4e458f33c3898ad2e35f
uscott <uriel.scott@protonmail.com>
2021-01-04 23:32:03 +0000

    Update test

--- test/websocket-private/ws_private_test.go
+++ test/websocket-private/ws_private_test.go
@@ -35,12 +35,8 @@ func TestStream_SubscribeToFills(t *testing.T) {
 		api.WithAuth(os.Getenv("FTX_PROD_MAIN_KEY"), os.Getenv("FTX_PROD_MAIN_SECRET")),
 	)
 	ctx, cancel := context.WithCancel(context.Background())
-	done := make(chan struct{})
-	go func() {
-		time.Sleep(sleepDuration)
-		cancel()
-		done <- struct{}{}
-	}()
+	defer cancel()
+
 	err := ftx.SetServerTimeDiff()
 	if err != nil {
 		t.Fatal(errors.WithStack(err))
@@ -51,22 +47,13 @@ func TestStream_SubscribeToFills(t *testing.T) {
 	if err != nil {
 		t.Fatal(errors.WithStack(err))
 	}
+
+	bid, _ := perp.Bid.Float64()
 	ask, _ := perp.Ask.Float64()
+
 	_, err = ftx.Orders.PlaceOrder(&models.OrderParams{
 		Market:   api.PtrString(swap),
 		Side:     api.PtrString(string(models.Buy)),
-		Price:    api.PtrDecimal(ask + 1),
-		Type:     api.PtrString(string(models.LimitOrder)),
-		Size:     api.PtrDecimal(0.001),
-		PostOnly: api.PtrBool(true),
-	})
-	if err != nil {
-		t.Fatal(errors.WithStack(err))
-	}
-	bid, _ := perp.Bid.Float64()
-	_, err = ftx.Orders.PlaceOrder(&models.OrderParams{
-		Market:   api.PtrString(swap),
-		Side:     api.PtrString(string(models.Sell)),
 		Price:    api.PtrDecimal(bid - 1),
 		Type:     api.PtrString(string(models.LimitOrder)),
 		Size:     api.PtrDecimal(0.001),
@@ -76,15 +63,41 @@ func TestStream_SubscribeToFills(t *testing.T) {
 		t.Fatal(errors.WithStack(err))
 	}
 
+	_, err = ftx.Orders.PlaceOrder(&models.OrderParams{
+		Market:   api.PtrString(swap),
+		Side:     api.PtrString(string(models.Sell)),
+		Price:    api.PtrDecimal(ask + 1),
+		Type:     api.PtrString(string(models.LimitOrder)),
+		Size:     api.PtrDecimal(0.001),
+		PostOnly: api.PtrBool(true),
+	})
+	if err != nil {
+		t.Fatal(errors.WithStack(err))
+	}
+
+	done := make(chan struct{})
+	go func() {
+		time.Sleep(sleepDuration)
+		cancel()
+		done <- struct{}{}
+	}()
 	for {
 		select {
 		case <-done:
+			if err = ftx.CancelAllOrders(&models.CancelAllParams{}); err != nil {
+				t.Fatal(errors.WithStack(err))
+			}
 			return
 		case msg := <-data:
-			if msg != nil {
+			if msg == nil {
+				t.Log("wat ....")
+				time.Sleep(time.Second)
+			} else {
 				t.Logf("Data: %+v\n", *msg)
 			}
 		default:
+			t.Log("waiting ...")
+			time.Sleep(time.Second)
 		}
 	}
 }
