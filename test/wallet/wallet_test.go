2d072d532b290720a52c2ba1d990716191253e44
US <uriel.scott@protonmail.com>
2021-01-06 04:27:41 -0600

    Write TestWallet_GetCoins

--- test/wallet/wallet_test.go
+++ test/wallet/wallet_test.go
@@ -1,11 +1,38 @@
 package test
 
 import (
+	"os"
 	"testing"
+
 	"github.com/pkg/errors"
 	"github.com/uscott/go-ftx/api"
-	"github.com/uscott/go-ftx/models"
 )
 
-func TestWallet_GetCoin(t *testing.T) {
+func prepForTest() (*api.Client, error) {
+
+	ftx := api.New(
+		api.WithAuth(os.Getenv("FTX_PROD_MAIN_KEY"), os.Getenv("FTX_PROD_MAIN_SECRET")),
+	)
+	if err := ftx.SetServerTimeDiff(); err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return ftx, nil
+}
+
+func TestWallet_GetCoins(t *testing.T) {
+
+	ftx, err := prepForTest()
+	if err != nil {
+		t.Fatal(err)
+	}
+	coins, err := ftx.Wallet.GetCoins()
+	if err != nil {
+		t.Fatal(errors.WithStack(err))
+	}
+	for i, c := range coins {
+		if i > 9 {
+			return
+		}
+		t.Logf("Coin: %+v\n", *c)
+	}
 }
