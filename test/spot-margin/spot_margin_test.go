252e2dc0ec7c24b4a0b2b0ed8dce0983c73a0d36
US <uriel.scott@protonmail.com>
2021-01-08 10:20:32 -0600

    Write spot margin tests

---
+++ test/spot-margin/spot_margin_test.go
@@ -0,0 +1,123 @@
+package test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/uscott/go-ftx/api"
+)
+
+func prepForTest(t *testing.T) *api.Client {
+
+	ftx := api.New(
+		api.WithAuth(os.Getenv("FTX_PROD_MAIN_KEY"), os.Getenv("FTX_PROD_MAIN_SECRET")),
+	)
+	if err := ftx.SetServerTimeDiff(); err != nil {
+		t.Fatal(errors.WithStack(err))
+	}
+	return ftx
+}
+
+func TestSpotMargin_GetBorrowRates(t *testing.T) {
+
+	ftx := prepForTest(t)
+
+	rates, err := ftx.SpotMargin.GetBorrowRates()
+	if err != nil {
+		t.Fatal(errors.WithStack(err))
+	}
+	for i, r := range rates {
+		if i > 9 {
+			return
+		}
+		t.Logf("Rate: %+v\n", *r)
+	}
+}
+
+func TestSpotMargin_GetLendingRates(t *testing.T) {
+
+	ftx := prepForTest(t)
+
+	rates, err := ftx.SpotMargin.GetLendingRates()
+	if err != nil {
+		t.Fatal(errors.WithStack(err))
+	}
+	for i, r := range rates {
+		if i > 9 {
+			return
+		}
+		t.Logf("Rate: %+v\n", *r)
+	}
+}
+
+func TestSpotMargin_GetBorrowSummary(t *testing.T) {
+
+	ftx := prepForTest(t)
+
+	summary, err := ftx.SpotMargin.GetBorrowSummary()
+	if err != nil {
+		t.Fatal(errors.WithStack(err))
+	}
+	t.Logf("Summary: %+v\n", *summary)
+}
+
+func TestSpotMargin_GetMarketInfo(t *testing.T) {
+
+	ftx := prepForTest(t)
+
+	info, err := ftx.SpotMargin.GetMarketInfo("BTC")
+	if err != nil {
+		t.Fatal(errors.WithStack(err))
+	}
+	t.Logf("Info: %+v\n", *info)
+}
+
+func TestSpotMargin_GetBorrowHistory(t *testing.T) {
+
+	ftx := prepForTest(t)
+
+	hist, err := ftx.SpotMargin.GetBorrowHistory()
+	if err != nil {
+		t.Fatal(errors.WithStack(err))
+	}
+	t.Logf("Info: %+v\n", *hist)
+}
+
+func TestSpotMargin_GetLendingHistory(t *testing.T) {
+
+	ftx := prepForTest(t)
+
+	hist, err := ftx.SpotMargin.GetLendingHistory()
+	if err != nil {
+		t.Fatal(errors.WithStack(err))
+	}
+	t.Logf("Info: %+v\n", *hist)
+}
+
+func TestSpotMargin_GetLendingOffers(t *testing.T) {
+
+	ftx := prepForTest(t)
+
+	offers, err := ftx.SpotMargin.GetLendingOffers()
+	if err != nil {
+		t.Fatal(errors.WithStack(err))
+	}
+	for i, o := range offers {
+		if i > 9 {
+			return
+		}
+		t.Logf("Lending offer: %+v\n", *o)
+	}
+}
+
+func TestSpotMargin_GetLendingInfo(t *testing.T) {
+
+	ftx := prepForTest(t)
+
+	info, err := ftx.SpotMargin.GetLendingInfo()
+	if err != nil {
+		t.Fatal(errors.WithStack(err))
+	}
+	t.Logf("Info: %+v\n", *info)
+}
