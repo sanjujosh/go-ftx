03f3cc6e63c13bc1d33268a2d496ab1b3f84d5a3
US <swanempire@protonmail.com>
2021-01-31 13:28:49 +0000

    Fix some errors

--- test/subaccounts/subaccounts_test.go
+++ test/subaccounts/subaccounts_test.go
@@ -23,26 +23,27 @@ func TestSubAccounts_CRUD(t *testing.T) {
 	nickname := "testSubAccount"
 	newNickname := "newTestSubAccount"
 
-	t.Run("getAll", func(t *testing.T) {
-		subs, err := ftx.SubAccounts.GetSubaccounts()
-		assert.NoError(t, err)
-		assert.NotNil(t, subs)
-		for _, sub := range subs {
-			require.True(t, sub.Nickname != nickname)
-			require.True(t, sub.Nickname != newNickname)
-			t.Logf("Subaccount: %+v\n", *sub)
-		}
-	})
-
-	t.Run("create", func(t *testing.T) {
-		sub, err := ftx.SubAccounts.CreateSubaccount(nickname)
-		assert.NoError(t, err)
-		assert.NotNil(t, sub)
-		assert.Equal(t, nickname, sub.Nickname)
-		assert.True(t, sub.Deletable)
-		assert.True(t, sub.Editable)
+	subs, err := ftx.SubAccounts.GetSubaccounts()
+	assert.NoError(t, err)
+	assert.NotNil(t, subs)
+	for _, sub := range subs {
 		t.Logf("Subaccount: %+v\n", *sub)
-	})
+		if sub.Nickname == nickname || sub.Nickname == newNickname {
+			_, err = ftx.SubAccounts.DeleteSubaccount(sub.Nickname)
+			if err != nil {
+				t.Fatal(err)
+
+			}
+		}
+	}
+
+	sub, err := ftx.SubAccounts.CreateSubaccount(nickname)
+	assert.NoError(t, err)
+	assert.NotNil(t, sub)
+	assert.Equal(t, nickname, sub.Nickname)
+	assert.True(t, sub.Deletable)
+	assert.True(t, sub.Editable)
+	t.Logf("Subaccount: %+v\n", *sub)
 
 	ftx.SubAccount = api.PtrString(nickname)
 	account := &models.AccountInformation{}
@@ -60,35 +61,29 @@ func TestSubAccounts_CRUD(t *testing.T) {
 		t.Logf("%+v\n", *p)
 	}
 
-	t.Run("get_balances", func(t *testing.T) {
-		balances, err := ftx.SubAccounts.GetSubaccountBalances(nickname)
-		assert.NoError(t, err)
-		assert.NotNil(t, balances)
-		for _, bal := range balances {
-			t.Logf("Balance: %+v\n", *bal)
-		}
-	})
+	ftx.SubAccount = nil
 
-	t.Run("update", func(t *testing.T) {
-		result, err := ftx.SubAccounts.ChangeSubaccount(nickname, newNickname)
-		assert.NoError(t, err)
-		t.Logf("Update result: %+v\n", result)
-	})
+	balances, err := ftx.SubAccounts.GetSubaccountBalances(nickname)
+	assert.NoError(t, err)
+	assert.NotNil(t, balances)
+	for _, bal := range balances {
+		t.Logf("Balance: %+v\n", *bal)
+	}
 
-	t.Run("delete", func(t *testing.T) {
-		result, err := ftx.SubAccounts.DeleteSubaccount(newNickname)
-		assert.NoError(t, err)
-		t.Logf("Delete result: %+v\n", result)
-	})
+	result, err := ftx.SubAccounts.ChangeSubaccount(nickname, newNickname)
+	assert.NoError(t, err)
+	t.Logf("Update result: %+v\n", result)
 
-	t.Run("check", func(t *testing.T) {
-		subs, err := ftx.SubAccounts.GetSubaccounts()
-		assert.NoError(t, err)
-		assert.NotNil(t, subs)
-		for _, sub := range subs {
-			require.True(t, sub.Nickname != nickname)
-			require.True(t, sub.Nickname != newNickname)
-			t.Logf("Check subaccount: %+v\n", *sub)
-		}
-	})
+	result, err = ftx.SubAccounts.DeleteSubaccount(newNickname)
+	assert.NoError(t, err)
+	t.Logf("Delete result: %+v\n", result)
+
+	subs, err = ftx.SubAccounts.GetSubaccounts()
+	assert.NoError(t, err)
+	assert.NotNil(t, subs)
+	for _, sub := range subs {
+		require.True(t, sub.Nickname != nickname)
+		require.True(t, sub.Nickname != newNickname)
+		t.Logf("Check subaccount: %+v\n", *sub)
+	}
 }
